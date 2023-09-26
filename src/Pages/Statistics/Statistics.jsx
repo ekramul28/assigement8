c655638bdb6d5f26458a9790f222ce1235f2e71c
Ekramul <mdekramulhassan168@gmail.com>
2023-09-26 15:17:01 +0600

    pie chart added

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import {
+    Chart as ChartJs,
+    ArcElement,
+    Tooltip,
+    Legend
+} from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { getItemOfLocalStor } from '../../utility/LocalStroage';
+ChartJs.register(
+    ArcElement,
+    Tooltip,
+    Legend
+);
 const Statistics = () => {
+    const item = getItemOfLocalStor();
+
+    const data = {
+        labels: ['Total Donation', 'Your Donation'],
+        datasets: [
+            {
+
+                backgroundColor: ['red', '#00C49F'],
+                data: [12, item.length]
+
+            }
+        ]
+    };
+
     return (
-        <div>
-            <h1>ok</h1>
+        <div className='App'>
+
+            <div className=' flex justify-center items-center '>
+                <div className='md:w-[500px] h-[700px] '>
+                    <Pie data={data}
+
+                    ></Pie>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/DonationCards/DonationCards.jsx
+++ src/components/DonationCards/DonationCards.jsx
@@ -5,7 +5,7 @@ const DonationCards = () => {
     const cards = useLoaderData();
 
     return (
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-2">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-2 md:pt-24 lg:pt-0">
 
             {
                 Array.isArray(cards) ? cards.map(card => <Card card={card} key={card.id}></Card>) : null}
